ac63349f56e11bcc05d8e56d594edee117efad74
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-22 22:59:59 +0100

    [add] footer elements

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,51 @@
+<footer>
+    <div class="container">
+        <div class="footer-top">
+            <div class="f-top-left">
+                <div class="list-wrap-2-in-1">
+                    <ul>
+                        <h2>dc comics</h2>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Tv</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <ul>
+                        <h2>SHOP</h2>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <ul>
+                    <h2>dc</h2>
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy Policy (news)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                <ul>
+                    <h2>sites</h2>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </div>
+            <div class="f-top-right">
+                <!-- <img src="../assets/img/dc-logo-bg.png" alt="" /> -->
+            </div>
+        </div>
+        <div class="footer-bottom"></div>
+    </div>
+</footer>
