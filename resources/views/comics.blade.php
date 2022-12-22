f0cb903c4dc7d47d4b4751f3bbc02048f47e6e1b
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-23 00:02:54 +0100

    [add] comic page

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+    <title>Laravel DC Comics</title>
+</head>
+
+<body>
+    @include('partials.header')
+    @include('partials.jumbotron')
+    <div class="app_main">
+        <div class="container">
+            <div class="card-wrapper">
+
+            </div>
+            <div class="cta">
+                <div class="button-show-more">show more</div>
+            </div>
+        </div>
+    </div>
+
+    @include('partials.footer')
+    @yield('content')
+</body>
+
+</html>
