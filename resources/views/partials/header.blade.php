2eb0145e07e3cb2089769ed39e818f0842058431
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-22 22:49:27 +0100

    [add] header with elements

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="container">
+        <div class="h-wrapper">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo') }}" alt="DC_LOGO" />
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li><a href="">characters</a></li>
+                    <li><a href="">comics</a></li>
+                    <li><a href="">movies</a></li>
+                    <li><a href="">tv</a></li>
+                    <li><a href="">games</a></li>
+                    <li><a href="">collectibles</a></li>
+                    <li><a href="">videos</a></li>
+                    <li><a href="">fans</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">shop</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
