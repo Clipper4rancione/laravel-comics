b1c026957da673208ce8d8ac769430dfa1160898
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-24 17:07:37 +0100

    [ADD] card.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <main>
-
+        
     </main>
 @endsection
---
+++ resources/views/partials/card.blade.php
@@ -0,0 +1,13 @@
+<div class="card-cont">
+    <div class="img-area">
+        <i class="bi bi-cart-plus"></i>
+
+        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
+            alt="comic.series" />
+    </div>
+
+    <div class="text-area">
+        <span>Action Comics</span>
+        <p>$19.99</p>
+    </div>
+</div>
