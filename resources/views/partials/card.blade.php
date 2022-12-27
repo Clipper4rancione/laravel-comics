3f019f890068ee16ed6417fbb5f51e3dfe54f74e
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-27 17:07:06 +0100

    prined card data from db.comics

--- resources/views/partials/card.blade.php
+++ resources/views/partials/card.blade.php
@@ -1,13 +1,14 @@
-<div class="card-cont">
-    <div class="img-area">
-        <i class="bi bi-cart-plus"></i>
+@foreach ($comics as $comic)
+    <div class="card-cont">
+        <div class="img-area">
+            <i class="bi bi-cart-plus"></i>
 
-        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-            alt="comic.series" />
-    </div>
+            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['thumb'] }}" />
+        </div>
 
-    <div class="text-area">
-        <span>Action Comics</span>
-        <p>$19.99</p>
+        <div class="text-area">
+            <span>{{ $comic['title'] }}</span>
+            <p>{{ $comic['price'] }}</p>
+        </div>
     </div>
-</div>
+@endforeach
